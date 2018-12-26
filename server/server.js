af77b09659c9afcff3d077520a651ed54a157049
ujjwalmishra21 <ujjwalmishra21@users.noreply.github.com>
2018-12-26 16:50:36 +0530

    welcome new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,14 +16,34 @@ app.use(express.static(publicPath));
 io.on('connection',function(socket){
   console.log('New user connected');
 
+  
+  socket.emit('newMessage',{
+    from:'admin',
+    text:'Welcome to ChatApp',
+    createdAt:new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage',{
+    from:'admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  })
+
+
   socket.on('createMessage',function (message) {
     console.log('Create Message', message);
-    
+
     io.emit('newMessage',{
       from:message.from,
       text:message.text,
       createdAt:new Date().getTime()
     })
+
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // })
   })
 
    socket.on('disconnect',function(){
