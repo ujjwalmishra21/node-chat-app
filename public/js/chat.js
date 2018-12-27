67fb80abb22d7240ab9df5f7f912c49fbcd38afa
ujjwalmishra21 <ujjwalmishra21@users.noreply.github.com>
2018-12-27 20:10:36 +0530

    user list updated

--- public/js/chat.js
+++ public/js/chat.js
@@ -57,6 +57,16 @@ socket.on('newLocationMessage',function (message) {
   scrollToBottom()
 })
 
+socket.on('updateUserList',function(users){
+  var ol = jQuery('<ol></ol>')
+
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+})
+
 socket.on('disconnect',function(){
   console.log("Disconnected from Server");
 })
--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -18,22 +22,21 @@ io.on('connection',function(socket){
 
   socket.on('join',(params, callback)=>{
     if(!isRealString(params.name) || !isRealString(params.room)){
-      callback('Name and room name are required')
+      return callback('Name and room name are required')
     }
 
     socket.join(params.room)
+    users.removeUser(socket.id)
+    users.addUser(socket.id, params.name, params.room)
     //socket.leave('class12') ---to leave a socket
 
     //io.emit ---> io.to('class12').emit
     //socket.broadcast.emit ----> socket.broadcast.to('class12').emit
     //socket.emit 
 
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room))
     socket.emit('newMessage',generateMessage('Admin','Welcome to ChatApp'));
-
     socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined`));
-
-
-
     callback()
   })
  
@@ -55,7 +58,12 @@ io.on('connection',function(socket){
   })
 
    socket.on('disconnect',function(){
-    console.log('User disconnected');
+    var user = users.removeUser(socket.id);
+    
+    if(user){
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room))
+      io.to(user.room).emit('newMessage',generateMessage('Admin', `${user.name} left.`))
+    }
   });
 
 });
